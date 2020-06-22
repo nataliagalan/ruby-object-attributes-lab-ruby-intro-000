ded5ad92b55deb251a1eb29d9fb674cab5ff6602
Natalia Galan <n8galan@gmail.com>
2020-06-22 17:57:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ You'll be teaching Dog about their names through two methods,
  #name, and #name= that read and write to a corresponding instance variable @name.
 
 class Dog
- 
+
   def name=(dog_name)
     @name = dog_name
   end
@@ -10,5 +10,5 @@ class Dog
   def name
     @name
   end
- 
+
 end
