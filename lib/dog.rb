b74999366af0af5e60a247e05ae20ad4044aaba5
Natalia Galan <n8galan@gmail.com>
2020-06-22 17:57:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,14 @@
-class Dog
-  def initialize(name)
-    @name = name
-  end
+You'll be teaching Dog about their names through two methods,
+ #name, and #name= that read and write to a corresponding instance variable @name.
 
+class Dog
+ 
+  def name=(dog_name)
+    @name = dog_name
+  end
+ 
   def name
     @name
   end
+ 
 end
