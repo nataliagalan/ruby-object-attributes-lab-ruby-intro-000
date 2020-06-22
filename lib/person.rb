5a340d1e1f5e2d3cea70444ab3385226a947cd5a
Natalia Galan <n8galan@gmail.com>
2020-06-22 18:01:34 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+#You'll be teaching Person about their names through two methods, 
+#name, and #name= that read and write to a corresponding instance variable @name
+
+class Person
+
+  def name= (person_name)
+    @name = person_name
+  end
+
+  def name 
+    @name
+  end
+  
+end
+
+
+  
